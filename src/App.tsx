c13c37bb02f653c446bb9d32142f61521abab724
Hassan Chawa <159958359+hes0o@users.noreply.github.com>
2026-02-17 16:16:36 +0300

    chore: initial project setup with Vite + React +TS

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/electron-vite.animate.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div>
+      <h1>web tasarim ve programlama</h1>
+      <h2>lab-1</h2>
+      <p> Ad soyad : hassan chawa</p>
+      <p> ogrenci no : 225541612</p>
+    </div>
+  )
+}
+
+export default App
